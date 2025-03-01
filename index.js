a303ca3755c4281f3535a991daea0b4e9d30850d
mrludvigi <mr.ludvigikhukhunaishvili@gmail.com>
2025-03-01 19:53:32 +0400

    discount

---
+++ index.js
@@ -0,0 +1,14 @@
+/*reverse ფასდაკლება
+ჩვენ შეგიქმნით ცვლადებს: */
+
+const total = 1500;
+const discount = 270;
+/*დათვალეთ რამდენ პროცენტიანი გამოდის ფასდაკლება და ჩაწერეთ ცვლადში:
+
+ let salePercentage;
+
+
+შედეგი დალოგეთ ეკრანზე */
+let salePercentage = (discount / total) * 100;
+
+console.log(salePercentage);
